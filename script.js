97ed24d87b786de7fd1b716fff48484eafe4f2be
mylinux010706-arch <mylinux010706@gmail.com>
2026-03-08 10:22:19 +0700

    Refactor face detection and ASCII rendering logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const video=document.getElementById("video")
+
 const ascii=document.getElementById("ascii")
 const process=document.getElementById("process")
 
@@ -10,79 +11,66 @@ const colorBtn=document.getElementById("color")
 
 let mode="bw"
 
-const chars="█▓▒@#MW8B&%$+=-:. "
+const chars="█▓▒@#MWB8&%$+=-:. "
 
 let faceDetector
 let faces=[]
 
-
-
-/* camera */
-
 navigator.mediaDevices.getUserMedia({
-
 video:{facingMode:"user"}
-
 }).then(stream=>{
-
 video.srcObject=stream
-
 })
 
-
-
-/* face detector */
-
 if("FaceDetector" in window){
-
-faceDetector=new FaceDetector({fastMode:true,maxDetectedFaces:5})
-
+faceDetector=new FaceDetector({fastMode:true,maxDetectedFaces:3})
 }
-
-
 
 async function detectFaces(){
-
 if(!faceDetector)return
-
 try{
-
 faces=await faceDetector.detect(video)
-
 }catch(e){}
-
 requestAnimationFrame(detectFaces)
-
 }
-
-
-
-/* setup */
 
 video.onloadeddata=()=>{
 
 ascii.width=640
 ascii.height=480
 
-process.width=80
-process.height=60
+process.width=40
+process.height=30
 
 detectFaces()
 
 draw()
-
 }
-
-
 
 bwBtn.onclick=()=>mode="bw"
 colorBtn.onclick=()=>mode="color"
 
+function insideFace(x,y){
 
+for(let f of faces){
+
+let box=f.boundingBox
+
+let fx=box.x/video.videoWidth*process.width
+let fy=box.y/video.videoHeight*process.height
+let fw=box.width/video.videoWidth*process.width
+let fh=box.height/video.videoHeight*process.height
+
+if(x>fx && x<fx+fw && y>fy && y<fy+fh){
+return true
+}
+
+}
+
+return false
+}
 
 function draw(){
-
-/* crop video supaya tidak gepeng */
 
 let vw=video.videoWidth
 let vh=video.videoHeight
@@ -96,40 +84,23 @@ let sw=vw
 let sh=vh
 
 if(videoRatio>targetRatio){
-
 sw=vh*targetRatio
 sx=(vw-sw)/2
-
 }else{
-
 sh=vw/targetRatio
 sy=(vh-sh)/2
-
 }
-
-
 
 pctx.drawImage(video,sx,sy,sw,sh,0,0,process.width,process.height)
 
 let frame=pctx.getImageData(0,0,process.width,process.height)
-
 let data=frame.data
-
-
 
 ctx.fillStyle="black"
 ctx.fillRect(0,0,ascii.width,ascii.height)
 
-
-
 let cw=ascii.width/process.width
 let ch=ascii.height/process.height
-
-ctx.font="bold "+ch+"px monospace"
-
-
-
-/* ascii render */
 
 for(let y=0;y<process.height;y++){
 
@@ -148,33 +119,40 @@ let char=chars[Math.floor(brightness/255*(chars.length-1))]
 let px=x*cw
 let py=y*ch
 
+let face=insideFace(x,y)
 
+if(face){
+
+ctx.font="bold "+(ch*50)+"px monospace"
+
+if(brightness>120){
+ctx.fillStyle="white"
+}else{
+ctx.fillStyle="white"
+char="█"
+}
+
+ctx.fillText(char,px,py)
+
+}else{
+
+ctx.font="bold "+ch+"px monospace"
 
 if(mode==="bw"){
 
-if(brightness<90){
-
-char="█"
-
-}else if(brightness<160){
-
-char="▓"
-
-}else{
-
-char="."
-
-}
-
+if(brightness>140){
 ctx.fillStyle="white"
+char="."
+}else{
+ctx.fillStyle="white"
+char="█"
+}
 
 }else{
 
-ctx.fillStyle=`rgb(${r},${g},${b})`
+ctx.fillStyle="rgb("+r+","+g+","+b+")"
 
 }
-
-
 
 ctx.fillText(char,px,py)
 
@@ -182,55 +160,7 @@ ctx.fillText(char,px,py)
 
 }
 
-
-
-/* FACE MASK (menutup wajah total) */
-
-faces.forEach(face=>{
-
-let box=face.boundingBox
-
-
-
-let fx=box.x/video.videoWidth*ascii.width
-let fy=box.y/video.videoHeight*ascii.height
-
-let fw=box.width/video.videoWidth*ascii.width
-let fh=box.height/video.videoHeight*ascii.height
-
-
-
-/* background hitam */
-
-ctx.fillStyle="black"
-
-ctx.fillRect(fx,fy,fw,fh)
-
-
-
-/* isi X */
-
-ctx.fillStyle="white"
-
-let size=20
-
-ctx.font="bold "+size+"px monospace"
-
-
-
-for(let y=fy;y<fy+fh;y+=size){
-
-for(let x=fx;x<fx+fw;x+=size){
-
-ctx.fillText("X",x,y)
-
 }
-
-}
-
-})
-
-
 
 requestAnimationFrame(draw)
 
